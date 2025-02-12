2244a1fda047e2699843290061966b1bc1d0d3b3
YurCorrea <yur.correa@gmail.com>
2025-02-11 21:21:00 -0300

    Criado projeto Management

---
+++ OeX.Management.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace OeX.Management.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ OeX.Management.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace OeX.Management.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
